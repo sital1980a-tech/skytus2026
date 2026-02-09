da152be57439e6fb82e02fbb13dd62c7625b2581
sital1980a-tech <sital1980a@gmail.com>
2026-02-08 18:15:37 -1000

    Add files via upload

---
+++ python_practical/pro1.py
@@ -0,0 +1,5 @@
+user_string = input("Enter a string: ")
+
+length = len(user_string)
+
+print("The length of the string is:", length)
---
+++ python_practical/pro9.py
@@ -0,0 +1,5 @@
+text = input("Enter a string with extra spaces: ")
+
+clean_text = text.strip()
+
+print("String after removing extra spaces:", clean_text)

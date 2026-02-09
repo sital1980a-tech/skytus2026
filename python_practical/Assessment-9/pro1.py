eb69ae467bd02a88dc80c31f0b8eeba9e18bd9e5
sital1980a-tech <sital1980a@gmail.com>
2026-02-08 20:17:16 -1000

    Create pro1.py

---
+++ python_practical/Assessment-9/pro1.py
@@ -0,0 +1,6 @@
+import mymath
+
+print("Addition:", mymath.add(10, 5))
+print("Subtraction:", mymath.subtract(10, 5))
+print("Multiplication:", mymath.multiply(10, 5))
+print("Division:", mymath.divide(10, 5))
